99d6fd0a4038674f75ea97c200f7bb668a78b69d
8aqtba9y <8aqtba9y@gmail.com>
2017-12-04 03:30:37 +0900

    [modify] preview

--- 25_Basic_word2vec/00_002_Basic_word2vec.py
+++ 25_Basic_word2vec/00_002_Basic_word2vec.py
@@ -5,6 +5,5 @@ model = word2vec.Word2Vec(data, size=200, window=10, hs=1, min_count=2, sg=1)
 model.save("toji.model")
 
 model = word2vec.Word2Vec.load("toji.model")
-print("positive=[\"땅\"]: ", model.most_similar(positive=["땅"]))
-print("positive=[\"이슬\"]: ", model.most_similar(positive=["이슬"]))
-print("positive=[\"땅\", \"이슬\"]: ", model.most_similar(positive=["땅", "이슬"]))
+print("positive=[\"서울\"]: ", model.most_similar(positive=["서울"])[:3])
+print("positive=[\"많다\", \"보통\", \"조금\"]: ", model.most_similar(positive=["많다", "보통", "조금"])[:1])
